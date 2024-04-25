19e4159b27b776ef1588ece713328521adaa70bd
Arvinth <b022220008@student.utem.edu.my>
2024-04-25 14:25:11 +0800

    hehe

--- index.js
+++ index.js
@@ -1,39 +1,113 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+  const express = require('express');
+  const bcrypt = require('bcrypt');
+  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(express.json())
+  const app = express();
+  const port = process.env.PORT || 3004; // Change the port number
 
-app.get('/', (req, res) => {
-   res.send('Hi there! Welcome to the MongoDB Atlas and Node.js example!')
-})
+  app.use(express.json());
 
-app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
+  //new user registration
+  app.post('/user:username', async (req, res) => {
+    //console.log(req.body);
+    const hash = bcrypt.hashSync(req.body.password, 10);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Vintoi:<@rvinthBALU30>@cluster0.p4r5qu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    //insertOne
+    let result = await client.db('Mlbb').collection('Heroes').insertOne({
+      username: req.body.username,  
+      password: hash,
+      name: req.body.name,
+      email: req.body.email
+    });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+    res.send(result);
+  });
+
+  app.post('/login', async (req, res) => {
+    //username: req.body.username
+    //password: req.body.password
+    //step 1: check if the user exists
+    client.db('Mlbb').collection('Heroes').findOne(
+      {username: req.body.username}
+    );
+      
+    if(!result){
+      res.send("User not found");
+      console.log(result); 
+    } else{
+      //step 2: check if the password is correct
+      //result.password = req.body.password
+      if (bcrypt.compareSync(req.body.password, result.password)== true){
+        res.send("Login successful");
+      } else{
+        res.send("Password is incorrect");
+      }
+    }
+  });
+
+  //get user profile
+  app.get('/user/:id',async (req, res) => {
+    let result = await client.db('Mlbb').collection('Heroes').findOne({
+    _id: new ObjectId(req.params.id)
+  })
+
+  res.send(result)
+  })
+    //findOne
+    //console.log("get user profile");
+
+  //update user account
+  app.patch('/user/:id', async (req, res) => {
+    let result = await client.db('Mlbb').collection('Heroes').updateOne(
+
+      {
+        _id: new ObjectId(req.params.id)
+      },
+      {
+        $set: {
+          name: req.body.name,
+        }
+      }
+
+    )
+
+    //updateOne
+    res.send(result)
+  });
+
+  //delete user account
+  app.delete('/user/:id', async(req, res) => {
+    //deleteOne
+    let result = await client.db('Mlbb').collection('Heroes').deleteOne(
+      {
+        _id: new ObjectId(req.params.id)
+      }
+    )
+    res.send(result)
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+
+
+  const uri = "mongodb+srv://Vintoi:Vintoi@cluster0.p4r5qu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      await client.connect();
+      console.log("Connected successfully to server");
+    } finally {
+      // Perform necessary actions, if any, when connection is closed
+    }
   }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+  run().catch(console.dir);
